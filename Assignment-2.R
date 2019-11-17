b0af23ce8ee3792e5678853bd93fd50302a8fd7e
Seel <459745355@qq.com>
2019-11-17 21:32:55 +0000

    R assignment

--- Assignment-2.R
+++ Assignment-2.R
@@ -1,5 +1,5 @@
 #%% Question 1
-single_test <- function(p_1,p_2,number.N1,number.N2,number.sample){
+getDefectiveProb <- function(p_1,p_2,number.N1,number.N2,number.sample){
     N1 = rbinom(n = number.N1, size = 1, prob = p_1)
     N2 = rbinom(n = number.N2, size = 1, prob = p_2)
     N = c(N1,N2)
@@ -16,16 +16,16 @@ number.N2 = 20000
 number.sample = 200
 simulation = 5000
 
-multi_test = replicate(n = simulation, expr = single_test(p_1,p_2,number.N1,number.N2,number.sample))
+MultiDefectiveProb = replicate(n = simulation, expr = getDefectiveProb(p_1,p_2,number.N1,number.N2,number.sample))
 
 #%%
-hist(multi_test)
+hist(MultiDefectiveProb)
 
 #%%
-quantile(multi_test,probs = c(0.025, 0.975))
+quantile(MultiDefectiveProb,probs = c(0.025, 0.975))
 
 #%%
-mean(multi_test)
+mean(MultiDefectiveProb)
 
 #%% True value
 (number.N1*p_1+number.N2*p_2)/(number.N1+number.N2)
@@ -35,19 +35,16 @@ N1 = 10000
 N2 = 20000
 p = 0.1
 e = 0.05
-sample = 134
 simulation = 50
 
-CalculateProb <- function(N1,N2,p,e,simulation,sample){
-  prob = mean(replicate(n = simulation, expr = abs(single_test(p,p,N1,N2,sample)-p)<e))
+getlessThanEProb <- function(N1,N2,p,e,simulation,sample){
+  prob = mean(replicate(n = simulation, expr = abs(getDefectiveProb(p,p,N1,N2,sample)-p)<e))
   return (prob)
 }
 
-#CalculateProb(N1,N2,p,e,simulation,114)
-
 library(parallel)
 RepParallel <- function(n, expr, simplify = "array",...){
-  mc <- getOption("mc.cores", 2)
+  mc <- getOption("mc.cores", detectCores())
   answer <-
     mclapply(integer(n), eval.parent(substitute(function(...) expr)),mc.cores = mc,...)
   if (!identical(simplify, FALSE) && length(answer))
@@ -55,37 +52,13 @@ RepParallel <- function(n, expr, simplify = "array",...){
   else return(answer)
 }
 
-#system.time({
-#  hist(replicate(500,CalculateProb(N1,N2,p,e,simulation,sample)))
-#})
-#system.time({
-#  hist(RepParallel(500,CalculateProb(N1,N2,p,e,simulation,sample)))
-#})
 
-#mean(RepParallel(50,CalculateProb(N1,N2,p,e,simulation,134)))
-top = N1+N2
-button = 1
-repeat{
-  pin_sample = ceiling((top-button)/2+button)
-  cat(i,'\n','pin',pin_sample,' button:',button,' top:',top,'\n')
-  pin_Pr = mean(RepParallel(200,CalculateProb(N1,N2,p,e,simulation,pin_sample)))
-  if (pin_Pr > 0.95){
-    top = pin_sample - 1
-  }
-  else{
-    button = pin_sample + 1
-  }
-  if (top == button) break
-}
-
-
-
-Q3_d <- function(p,e,N1,N2,simulation){
+getSample <- function(p,e,N1,N2){
   top = N1+N2
   button = 1
   while(button<top){
     pin_sample = ceiling((top-button)/2+button)
-    pin_Pr = mean(RepParallel(50,CalculateProb(N1,N2,p,e,simulation,pin_sample)))
+    pin_Pr = mean(RepParallel(20,getlessThanEProb(N1,N2,p,e,30,pin_sample)))
     if (pin_Pr > 0.95){
       top = pin_sample - 1
     }
@@ -97,31 +70,16 @@ Q3_d <- function(p,e,N1,N2,simulation){
 }
 
 system.time({
-  ans <- Q3_d(p,e,N1,N2,simulation)
+  ans <- getSample(p,e,N1,N2)
 })
 ans
 
-sample.distribution = replicate(n = 400, expr = Q3_d(p,e,N1,N2,simulation))
+sample.distribution = replicate(n = 40, expr = getSample(p,e,N1,N2))
 mean(sample.distribution)
 hist(sample.distribution)
 
-mean(sample.distribution)
 
-Q3 <- function(p,e,N1,N2,simulation){
-  N = N1+N2
-  for (sample in 120:140){
-    mean_Pr = mean(RepParallel(200,CalculateProb(N1,N2,p,e,simulation,sample)))
-    if (mean_Pr>0.95){
-      cat(sample,' ',mean_Pr)
-      return (sample)
-    }
-  }
-}
+rbinom(n = 10, size = 1, prob = 0.5)
 
-Q3(p,e,N1,N2,simulation)
 
-sample.distribution = replicate(n = 10, expr = Q3(p,e,N1,N2,simulation))
-hist(sample.distribution)
-mean(sample.distribution)
-sample.distribution
-quantile(sample.distribution,probs = c(0.025, 0.975))
+ceiling(3.1)
